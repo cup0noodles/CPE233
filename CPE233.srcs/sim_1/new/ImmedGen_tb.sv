d657be21958d7a8954f7e0f8f1104cb4c25f3697
mkw_pop_laptop <mkw_pop_laptop@pop-os.localdomain>
2022-01-26 14:05:43 -0800

    Edited ImmedGen

--- CPE233.srcs/sim_1/new/ImmedGen_tb.sv
+++ CPE233.srcs/sim_1/new/ImmedGen_tb.sv
@@ -1,26 +1,58 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/25/2022 01:47:01 PM
-// Design Name: 
-// Module Name: ImmedGen_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module ImmedGen_tb();
 
-module ImmedGen_tb(
-
-    );
+    logic [31:0] INS, U, I, S, B, J;
+    
+    ImmedGen UUT(.INSTRUCT(INS), .U_TYPE(U),
+                 .I_TYPE(S), .B_TYPE(B), 
+                 .J_TYPE(J));
+    
+    initial begin
+        // U Type Test
+            #10 INS = {'hABCDEF, 1'b0, 7'b0110111};
+            #10 INS = {'h123456, 1'b0, 7'b0110111};
+        
+        //I Type Test
+            //13 bits should be ignored
+            //Sign Extend 1
+            #10 INS = {1'b1, 11'b11001100110, 13'b1111111111111, 7'b0010011};
+            //Sign Extend 0
+            #10 INS = {1'b0, 11'b11001100110, 13'b1111111111111, 7'b0010011};
+        
+        //S Type Test
+            //Sign Extend 1
+            //Check for 13-bits ignored
+            #10 INS = {1'b1, 6'b000000, 13'b1111111111111, 5'b00000, 7'b0100011};
+            //Sign Extend 0
+            //Check for IR[30:25]
+            #10 INS = {1'b0, 6'b010101, 13'b1111111111111, 5'b00000, 7'b0100011};
+            //Check for IR[11:7]
+            #10 INS = {1'b0, 6'b000000, 13'b1111111111111, 5'b01010, 7'b0100011};
+        
+        //B Type Test
+            //Sign Extend 0
+            //Check for IR[7]
+            //13-bits should be ignored
+            #10 INS = {1'b0, 6'b000000, 13'b1111111111111, 4'b0000, 1'b1, 7'b1100011};
+            //Check for IR[30:25]
+            #10 INS = {1'b0, 6'b101010, 13'b1111111111111, 4'b0000, 1'b0, 7'b1100011};
+            //Check for IR[11:8]
+            #10 INS = {1'b0, 6'b000000, 13'b1111111111111, 4'b1010, 1'b0, 7'b1100011};
+            //Sign Extend 1
+            #10 INS = {1'b1, 6'b000000, 13'b1111111111111, 4'b0000, 1'b0, 7'b1100011};
+        
+        //J Type Test
+            //Sign Extend 0
+            // 5-bits should be ignored
+            //Check for IR[19:12]
+            #10 INS = {1'b0, 10'b0000000000, 1'b0, 8'b10101010, 5'b11111, 7'b1101111};
+            //Check for IR[20]
+            #10 INS = {1'b0, 10'b0000000000, 1'b1, 8'b00000000, 5'b11111, 7'b1101111};
+            //Check for IR[30:21]
+            #10 INS = {1'b0, 10'b1010101010, 1'b0, 8'b00000000, 5'b11111, 7'b1101111};
+            //Sign Extend 1
+            #10 INS = {1'b1, 10'b0000000000, 1'b0, 8'b00000000, 5'b00000, 7'b1101111};
+    end
+    
 endmodule
--- CPE233.srcs/sources_1/new/ImmedGen.sv
+++ CPE233.srcs/sources_1/new/ImmedGen.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Cal Poly
+// Engineer: Matthew Wong
 // 
 // Create Date: 01/25/2022 01:46:19 PM
 // Design Name: 
@@ -21,11 +21,57 @@
 
 
 module ImmedGen(
-    input [31:0] INSTRUCT,
-    output [31:0] U_TYPE,
-    output [31:0] I_TYPE,
-    output [31:0] S_TYPE,
-    output [31:0] J_TYPE,
-    output [31:0] B_TYPE
+    input logic [31:0] INSTRUCT,
+    output logic [31:0] U_TYPE,
+    output logic [31:0] I_TYPE,
+    output logic [31:0] S_TYPE,
+    output logic [31:0] J_TYPE,
+    output logic [31:0] B_TYPE
     );
+    
+    logic [6:0] opcode;
+    
+    always_comb begin
+        opcode = INSTRUCT[6:0]; //Load opcode type from ins
+        case (opcode)
+            7'b0010011: begin
+            //I_TYPE
+                I_TYPE = {{21{INSTRUCT[31]}}, {INSTRUCT[30:20]}};
+            end
+            
+            7'b0100011: begin
+            //S_TYPE
+                S_TYPE = {{21{INSTRUCT[31]}}, {INSTRUCT[30:25]}, {INSTRUCT[11:7]}};
+            end
+            
+            7'b1100011: begin
+            //B_TYPE
+                B_TYPE = {{20{INSTRUCT[31]}}, {INSTRUCT[7]}, 
+                          {INSTRUCT[30:25]}, {INSTRUCT[11:8]}, 
+                          1'b0};
+            end
+            
+            7'b0110111: begin
+            //U_TYPE
+                U_TYPE = {{20{INSTRUCT[31:12]}}, {12{1'b0}}};
+            end
+            
+            7'b1101111: begin
+            //J_TYPE
+                J_TYPE = {{12{INSTRUCT[31]}}, {INSTRUCT[19:12]},
+                          {2{INSTRUCT[20]}}, INSTRUCT[30:21], 
+                          1'b0};
+            end
+            
+            default: begin
+            //Error Condition
+                U_TYPE = 0;
+                I_TYPE = 0;
+                J_TYPE = 0;
+                S_TYPE = 0;
+                B_TYPE = 0;
+            end
+        endcase
+    end
+    
 endmodule
